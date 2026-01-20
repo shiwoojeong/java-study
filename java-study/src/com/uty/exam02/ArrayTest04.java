4f6400b99b380c34ccf09cd62de595483cbf1c1b
eynow <eynow1159@gmail.com>
2026-01-20 12:18:22 +0900

    Array 실습 그래프 그리기

---
+++ java-study/src/com/uty/exam02/ArrayTest04.java
@@ -0,0 +1,40 @@
+package com.uty.exam02;
+
+public class ArrayTest04 {
+	public static void main(String[] args) {
+		int sales[] = { 52, 62, 30, 95, 77, 0, 100 };
+		char graph[][] = new char[10][7];
+
+		for (int i = 0; i < graph.length; i++) {
+			for (int j = 0; j < graph[i].length; j++) {
+				if ((graph.length - sales[j] / graph.length) > i) {
+					graph[i][j] = '.';
+				} else {
+					graph[i][j] = '0';
+				}
+
+			}
+		}
+		
+//		for (int i = 0; i < graph.length; i++)
+//			for (int j = 0; j < graph[0].length; j++)
+//				graph[i][j] = '.';
+//
+//		for (int i = 0; i < sales.length; i++) {
+//			for (int k = 0; k < sales[i] / 10; k++) {
+//				graph[graph.length - 1 - k][i] = 'O';
+//			}
+//		}
+		
+		System.out.println("-----<그래프>------");
+		for (int i = 0; i < graph.length; i++) {
+			for (int j = 0; j < graph[0].length; j++) {
+				System.out.printf("%c ", graph[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("-------------------");
+		System.out.println("1 2 3 4 5 6 7 ");
+
+	}
+}
