70d85c98b9e53cdf81b2eccc45e2606f2a12275d
eynow <eynow1159@gmail.com>
2026-01-20 14:20:04 +0900

    ,,

--- java-study/src/com/uty/exam03/Car.java
+++ java-study/src/com/uty/exam03/Car.java
@@ -1,6 +1,7 @@
 package com.uty.exam03;
 
 public class Car {
+	
 	String model = "new car";
 	String color = "white";
 	
@@ -8,6 +9,13 @@ public class Car {
 		model = inputModel;
 		color = inputColor;
 	}
+	
+	public Car(String model, String color) {
+		super();
+		this.model = model;
+		this.color = color;
+	}
+	
 	void drive() {
 		System.out.println(model + "(" + color + ")" + " driving~~~~~");
 	}
