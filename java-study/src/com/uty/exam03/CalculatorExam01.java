aee019ee7088501c1ec41b5e887a021c548d9f9f
eynow <eynow1159@gmail.com>
2026-01-20 15:01:44 +0900

    method

---
+++ java-study/src/com/uty/exam03/CalculatorExam01.java
@@ -0,0 +1,12 @@
+package com.uty.exam03;
+
+public class CalculatorExam01 {
+
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		int num1 = 10;
+		int num2 = 20;
+		System.out.println("num1 + num2 = " + cal.add(num1, num2));
+	}
+
+}
