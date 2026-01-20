361070878ecb95db6464121385c6788e06f3cfb1
eynow <eynow1159@gmail.com>
2026-01-20 13:36:55 +0900

    객체지향 실습

---
+++ java-study/src/com/uty/exam02/Car.java
@@ -0,0 +1,12 @@
+package com.uty.exam02;
+
+public class Car {
+	String model = "new car";
+	String color = "white";
+	
+	void drive() {
+		System.out.println("driving~~~~~");
+	}
+
+	
+}
---
+++ java-study/src/com/uty/exam02/CarExam.java
@@ -0,0 +1,10 @@
+package com.uty.exam02;
+
+public class CarExam {
+	public static void main(String[] args) {
+		Car myCar = new Car();
+
+		myCar.drive();
+		
+	}
+}
