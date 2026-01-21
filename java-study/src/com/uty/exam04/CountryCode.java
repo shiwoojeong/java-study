521a9f84dda173d50b0626279f46fa6e015de085
eynow <eynow1159@gmail.com>
2026-01-21 10:45:31 +0900

    자바 상수

---
+++ java-study/src/com/uty/exam04/CountryCode.java
@@ -0,0 +1,9 @@
+package com.uty.exam04;
+
+
+public class CountryCode {
+	public static final String KOREA = "+82";
+	public static final String USA = "+1";
+	public static final String SPAIN = "+34";
+	public static final String MALAYSIA = "+60";
+}
---
+++ java-study/src/com/uty/exam04/CountryCodeExam.java
@@ -0,0 +1,8 @@
+package com.uty.exam04;
+
+public class CountryCodeExam {
+	public static void main(String[] args) {
+		System.out.println("말레이시아 국가번호: " + CountryCode.MALAYSIA);
+		System.out.println("스페인 국가번호: " + CountryCode.SPAIN);
+	}
+}
