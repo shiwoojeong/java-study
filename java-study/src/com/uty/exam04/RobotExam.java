e5e81a9a83395aeadad2e2399c22c002efc913d3
eynow <eynow1159@gmail.com>
2026-01-21 10:26:14 +0900

    static

--- java-study/src/com/uty/exam04/RobotExam.java
+++ java-study/src/com/uty/exam04/RobotExam.java
@@ -7,8 +7,12 @@ public class RobotExam {
 		Robot robot03 = new Robot("robot-03", "assistance");
 		
 		System.out.println("Number of Robots: " + Robot.getPopulation());
+		
 	}
 }
 
 //Java applicaton 기동 순서
 //Class Loader 기동 -> main 쓰레드 기동
+//main 매서드는 무조건 public static이어야한다.
+//public이 아니면 main쓰레드가 접근하지 못한다
+//static이 아니면 객체를 만들어야 접근 가능하다
