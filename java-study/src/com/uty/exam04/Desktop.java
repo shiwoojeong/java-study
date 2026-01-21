5287426cfe27c148dfb270ef987d99439df997a1
eynow <eynow1159@gmail.com>
2026-01-21 10:29:37 +0900

    static block

---
+++ java-study/src/com/uty/exam04/Desktop.java
@@ -0,0 +1,16 @@
+package com.uty.exam04;
+
+public class Desktop {
+	private static String cpu = "8 core";
+	private static String memory = "RAM 32GB";
+	private static String video = "RTX 3060 8GB";
+	private static String feature;
+	
+	static {	//static block
+		feature = cpu + "/" + memory + "/" + video;
+	}
+
+	public static String getFeature() {
+		return feature;
+	}
+}
---
+++ java-study/src/com/uty/exam04/DesktopExam.java
@@ -0,0 +1,10 @@
+package com.uty.exam04;
+
+public class DesktopExam {
+
+	public static void main(String[] args) {
+
+		System.out.println(Desktop.getFeature());
+	}
+
+}
