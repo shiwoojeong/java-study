cf83782ffd0880aa33e0c9252631aec799dd3997
eynow <eynow1159@gmail.com>
2026-01-22 14:12:11 +0900

    중첩 클래스

---
+++ java-study/src/com/uty/exam08/A.java
@@ -0,0 +1,24 @@
+package com.uty.exam08;
+
+public class A {
+	public void method1(int arg) {
+		int var=1;
+		
+		class B {
+			void method2() {
+				System.out.println("arg: " + arg);
+				System.out.println("var: " + var);
+				
+				arg =3;
+				var =100;
+			}
+		}
+		B b = new B();
+		b.method2();
+		
+		arg =3;
+		var =100;
+	}
+	
+	
+}
---
+++ java-study/src/com/uty/exam08/Aexam.java
@@ -0,0 +1,8 @@
+package com.uty.exam08;
+
+public class Aexam {
+	public static void main(String[] args) {
+		A a = new A();
+		a.method1(10);
+	}
+}
