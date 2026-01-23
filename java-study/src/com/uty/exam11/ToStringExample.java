83d30b83b3f45566fe63258533d0143735ff5232
eynow <eynow1159@gmail.com>
2026-01-23 11:01:58 +0900

    tostring

---
+++ java-study/src/com/uty/exam11/ToStringExample.java
@@ -0,0 +1,12 @@
+package com.uty.exam11;
+
+public class ToStringExample {
+	public static void main(String[] args) {
+		SmartPhone myPhone = new SmartPhone("삼성전자", "안드로이드");
+
+		String strObj = myPhone.toString();
+		
+		System.out.println(strObj);
+		System.out.println(myPhone);
+	}
+}
