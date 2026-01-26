45da1fccb0e99922a4ed46300a9d14c47824c5c6
eynow <eynow1159@gmail.com>
2026-01-26 15:30:00 +0900

    와일드카드

---
+++ java-study/src/com/uty/exam13/step04/Person.java
@@ -0,0 +1,16 @@
+package com.uty.exam13.step04;
+public class Person {
+}
+
+class Worker extends Person {
+}
+
+class Student extends Person {
+}
+
+class HighStudent extends Student {
+}
+
+class MiddleStudent extends Student{
+}
+
